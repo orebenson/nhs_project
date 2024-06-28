9551087fd0fb87d8766731009c88a871513e4840
Ore Benson <bensono1@cardiff.ac.uk>
2024-06-28 12:33:22 +0100

    Initialized Spring Boot project

---
+++ src/main/java/nhs/uhdb/NHS_project/NhsProjectApplication.java
@@ -0,0 +1,13 @@
+package nhs.uhdb.NHS_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nhs/uhdb/NHS_project/NhsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nhs.uhdb.NHS_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
