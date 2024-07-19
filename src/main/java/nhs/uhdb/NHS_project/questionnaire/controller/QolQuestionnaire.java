b579e7e9f54d25b6d67476f09709c9371a870730
James Pang <Pangjy@cardiff.ac.uk>
2024-07-19 04:34:36 +0100

    questionnaire updates

--- src/main/java/nhs/uhdb/NHS_project/questionnaire/controller/QolQuestionnaire.java
+++ src/main/java/nhs/uhdb/NHS_project/questionnaire/controller/QolQuestionnaire.java
@@ -1,1 +1,20 @@
 package nhs.uhdb.NHS_project.questionnaire.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class QolQuestionnaire {
+
+    @GetMapping("/taskhub/qol-questionnaire")
+    public ModelAndView questionnaire() {
+        return new ModelAndView("questionnaires/qolQuestionnaire");
+    }
+
+//    @PostMapping("/questionnaires")
+//    public ModelAndView submitForm(){
+//    return new
+//    }
+
+}
