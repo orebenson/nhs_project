7e1a360f4ca19ce7f7b4fa3e2a8ce4c2faffafc1
Qiming <wangq79@cardiff.ac.uk>
2024-07-29 02:07:25 +0100

    Add controllers, some connections

---
+++ src/main/java/nhs/uhdb/NHS_project/admin/controllers/AdminGoalController.java
@@ -0,0 +1,21 @@
+package nhs.uhdb.NHS_project.admin.controllers;
+
+import nhs.uhdb.NHS_project.accounts.model.User;
+import nhs.uhdb.NHS_project.accounts.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminGoalController {
+
+    private UserService userService;
+
+    @GetMapping("/admin/{user_id}/goalSetting")
+    public ModelAndView adminGoalSetting(@PathVariable("user_id") int user_id) {
+        ModelAndView modelAndView = new ModelAndView("admin/adminSetUserGoal");
+        User user = userService.getUserByUserId(user_id);
+        return modelAndView;
+    }
+}
